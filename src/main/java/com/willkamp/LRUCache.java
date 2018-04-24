6211e298e416c14e12729ab211f4845184d70c13
Will Kamp <will.kamp@nordstrom.com>
2018-04-24 08:54:51 -0700

    simple solution

--- src/main/java/com/willkamp/LRUCache.java
+++ src/main/java/com/willkamp/LRUCache.java
@@ -1,39 +1,48 @@
 package com.willkamp;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.Collections;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
 
 public class LRUCache<K, V> {
 
+    private final Impl<K, V> implementation;
+
     public LRUCache(int capacity) {
+        implementation = new Impl<>(capacity);
     }
 
     Optional<V> get(K key) {
-        //todo: implement me
-        return Optional.empty();
+        return Optional.ofNullable(implementation.get(key));
     }
 
     V get(K key, Supplier<V> factory) {
-        //todo: implement me
-        throw new NotImplementedException();
+        V value = get(key).orElseGet(factory);
+        implementation.put(key, value);
+        return value;
     }
 
     void set(K key, V value) {
-        //todo: implement me
+        implementation.put(key, value);
     }
 
     boolean contains(K key) {
-        //todo: implement me
-        throw new NotImplementedException();
+        return implementation.containsKey(key);
     }
 
-    List<V> values() {
-        //todo: implement me
-        return Collections.emptyList();
+    private static class Impl<K, V> extends LinkedHashMap<K, V> {
+        private final int cacheSize;
+
+        private Impl(int cacheSize) {
+            super((int) Math.ceil(cacheSize * 1.25f), 0.75f, true);
+            this.cacheSize = cacheSize;
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+            return size() > cacheSize;
+        }
     }
 
 }
